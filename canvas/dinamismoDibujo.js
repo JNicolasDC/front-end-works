17007c4bb649c75e80cde6a3f5d9093e7f563f2e
JNicolasDC <nicolasdeazac@gmail.com>
2021-09-27 19:25:40 -0500

    Adding a function

--- canvas/dinamismoDibujo.js
+++ canvas/dinamismoDibujo.js
@@ -12,3 +12,17 @@ lienzo.lineTo(300,300);
 lienzo.lineTo(300,100);
 lienzo.stroke();
 lienzo.closePath();
+
+
+//Drawing with a function
+function createDraws(color, xInicial, yInicial, xFinal, yFinal){
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.moveTo(xInicial,yInicial);
+    lienzo.lineTo(xFinal,yFinal);
+    lienzo.stroke();
+    lienzo.closePath();
+}
+
+createDraws("purple", 50, 350, 350, 350);
+
