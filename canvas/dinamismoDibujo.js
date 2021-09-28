337892282e6a40fe82d72023deb8a1956a9fc75c
JNicolasDC <nicolasdeazac@gmail.com>
2021-09-27 21:23:08 -0500

    cycles with functions

--- canvas/dinamismoDibujo.js
+++ canvas/dinamismoDibujo.js
@@ -1,15 +1,22 @@
-alert('Test')
+//alert('Test')
 
 var d = document.getElementById("dibujoCanvas");
 var lienzo = d.getContext("2d");
 console.log(lienzo);
 
+var lineas = 400;
+var l = 0;
+var l2 = 0;
+
+var yI, xF;
+var xI, yF;
+
 lienzo.beginPath();
 lienzo.strokeStyle = "purple";
-lienzo.moveTo(100,300);
-lienzo.lineTo(100,100);
-lienzo.lineTo(300,300);
-lienzo.lineTo(300,100);
+lienzo.moveTo(150,250);
+lienzo.lineTo(150,150);
+lienzo.lineTo(250,250);
+lienzo.lineTo(250,150);
 lienzo.stroke();
 lienzo.closePath();
 
@@ -24,5 +31,21 @@ function createDraws(color, xInicial, yInicial, xFinal, yFinal){
     lienzo.closePath();
 }
 
-createDraws("purple", 50, 350, 350, 350);
+createDraws("purple", 150, 275, 250, 275);
 
+
+while(l<lineas){
+    yI = 10*l;
+    xF = 10*(l+1);
+    createDraws("blue", 0, yI, xF, 400);
+    console.log("Linea: " + l);
+    l = l + 1;
+}
+
+while(l2<lineas){
+    xI = 10*l2;
+    yF = 10*(l2+1);
+    createDraws("geen", xI, 0, 400, yF);
+    console.log("Linea: " + l2);
+    l2 = l2 + 1;
+}
