a3cf5c5c4d6ef68737559b812f2a9fe80150b7c8
JNicolasDC <nicolasdeazac@gmail.com>
2021-10-05 20:49:38 -0500

    Obtaining data from a form

--- canvas/dinamismoDibujo.js
+++ canvas/dinamismoDibujo.js
@@ -1,23 +1,13 @@
-//alert('Test')
+var texto = document.getElementById("text_lines");
+var boton = document.getElementById("button");
+boton.addEventListener("click", drawingWhenClick);
+
 
 var d = document.getElementById("dibujoCanvas");
 var lienzo = d.getContext("2d");
 console.log(lienzo);
-
-var lineas = 400;
-var l = 0;
-
-var yI, xF;
-var xI, yF;
-
-lienzo.beginPath();
-lienzo.strokeStyle = "purple";
-lienzo.moveTo(150,250);
-lienzo.lineTo(150,150);
-lienzo.lineTo(250,250);
-lienzo.lineTo(250,150);
-lienzo.stroke();
-lienzo.closePath();
+var anchoLienzo = d.width;
+console.log(anchoLienzo);
 
 
 //Drawing with a function
@@ -30,20 +20,42 @@ function createDraws(color, xInicial, yInicial, xFinal, yFinal){
     lienzo.closePath();
 }
 
-createDraws("purple", 150, 275, 250, 275);
+//
+function drawingWhenClick(){
+    //var x = parseInt(texto.value);
+    alert("Oprimiste el boton " + lineas);
 
+    var lineas = parseInt(texto.value);
+    var l = 0;
 
-while(l<lineas){
-    yI = 10*l;
-    xF = 10*(l+1);
-    createDraws("blue", 0, yI, xF, 400);
-    console.log("Linea: " + l);
-    l++;
-}
+    var yI, xF;
+    var xI, yF;
 
-for(l2=0 ; l2<lineas ; l2++){
-    xI = 10*l2;
-    yF = 10*(l2+1);
-    createDraws("geen", xI, 0, 400, yF);
-    console.log("Linea: " + l2);
+    var lineSpacing = anchoLienzo/lineas;
+
+    lienzo.beginPath();
+    lienzo.strokeStyle = "purple";
+    lienzo.moveTo(150,250);
+    lienzo.lineTo(150,150);
+    lienzo.lineTo(250,250);
+    lienzo.lineTo(250,150);
+    lienzo.stroke();
+    lienzo.closePath();
+
+    createDraws("purple", 150, 275, 250, 275);
+
+    while(l<lineas){
+        yI = lineSpacing*l;
+        xF = lineSpacing*(l+1);
+        createDraws("blue", 0, yI, xF, 400);
+        console.log("Linea: " + l);
+        l++;
+    }
+
+    for(l2=0 ; l2<lineas ; l2++){
+        xI = lineSpacing*l2;
+        yF = lineSpacing*(l2+1);
+        createDraws("geen", xI, 0, 400, yF);
+        console.log("Linea: " + l2);
+    }
 }
